f486fa68457c74f4abb0be04d757f171a4a30b20
Claudia D'Adamo <claudiadadamo@gmail.com>
2014-11-08 22:26:12 -0500

    added functionality for if there are multiple destinations - ex. if there is more than 1 warm loc

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 	}
 
 	var getDestination = function(){
-		return "NCE";
+		return ["LAX","NCE"];
 	}
 
 	var getDepartureDate = function() {
@@ -25,15 +25,20 @@ $(document).ready(function () {
 	// 	request = "http://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?origin=" + getOrigin + "&departure_date=" + getDepartureDate + "&duration=1--30&max_price=" + getMaxPrice + "&apikey=nRLZ1a7XwQyUiepJflPOx1djGdUo9bGf";
 	// 	return request;
 	// }
-
-	flights = $.ajax({
-		url: "http://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?origin=" + getOrigin() + "&departure_date=" + getDepartureDate() + "&duration=1--30&max_price=" + getMaxPrice() + "&apikey=nRLZ1a7XwQyUiepJflPOx1djGdUo9bGf",
+	for (var i = 0; i < getDestination().length; i++) {
+		flights = $.ajax({
+		url: "http://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?origin=" + getOrigin() + "&destination=" + getDestination()[i] + "&departure_date=" + getDepartureDate() + "&duration=1--30&max_price=" + getMaxPrice() + "&apikey=nRLZ1a7XwQyUiepJflPOx1djGdUo9bGf",
 		dataType: 'json',
 	}).done(function( data ){
-		console.log(data.results[1].airline);
-
-
+		console.log(data);
 	});
+	};
+	// flights = $.ajax({
+	// 	url: "http://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?origin=" + getOrigin() + "&destination=" + getDestination() + "&departure_date=" + getDepartureDate() + "&duration=1--30&max_price=" + getMaxPrice() + "&apikey=nRLZ1a7XwQyUiepJflPOx1djGdUo9bGf",
+	// 	dataType: 'json',
+	// }).done(function( data ){
+	// 	console.log(data);
+	// });
 	//console.log(flights);
 
 });
