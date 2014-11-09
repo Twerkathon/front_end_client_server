bffdcad38aa9393a0eab14cc1f20f7181835ce24
Claudia D'Adamo <claudiadadamo@gmail.com>
2014-11-08 21:40:20 -0500

    dealing with apis

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+	var getOrigin = function(){
+		return "BOS";
+	}
+
+	var getDestination = function(){
+		return "NCE";
+	}
+
+	var getDepartureDate = function() {
+		return "2015-09-08--2015-10-12";
+		// flexible or not
+	}
+
+	var getMaxPrice = function(){
+		return 5000;
+	}
+
+	// var aggregationMode = function(){
+	// 	return 
+	// }
+
+	// var concat = function(getOrigin, getDestination, getDepartureDate, getDuration, getMaxPrice) {
+	// 	request = "http://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?origin=" + getOrigin + "&departure_date=" + getDepartureDate + "&duration=1--30&max_price=" + getMaxPrice + "&apikey=nRLZ1a7XwQyUiepJflPOx1djGdUo9bGf";
+	// 	return request;
+	// }
+
+	flights = $.ajax({
+		url: "http://api.sandbox.amadeus.com/v1.2/flights/inspiration-search?origin=" + getOrigin() + "&departure_date=" + getDepartureDate() + "&duration=1--30&max_price=" + getMaxPrice() + "&apikey=nRLZ1a7XwQyUiepJflPOx1djGdUo9bGf",
+	}).done(function( data ){
+		return data.responseJSON.results;
+
+
+	});
+	console.log(flights);
+
+});
+
+
